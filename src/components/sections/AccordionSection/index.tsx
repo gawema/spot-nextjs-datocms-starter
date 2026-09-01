6868c3fd4ca1e4d1576e945cdd92d45315459636
gawema <gwmannucci@gmail.com>
2026-09-01 16:52:41 +0200

    refactor: update SectionContainer width to 'content' for Accordion and Slider sections

--- src/components/sections/AccordionSection/index.tsx
+++ src/components/sections/AccordionSection/index.tsx
@@ -16,7 +16,7 @@ export default function AccordionSection({ data, locale }: Props) {
   const { heading, items } = readFragment(AccordionSectionFragment, data);
 
   return (
-    <SectionContainer className="padding-vertical-main" width="measure">
+    <SectionContainer className="padding-vertical-main" width="content">
       {heading ? <h2 className="accordion-section__heading">{heading}</h2> : null}
       <div className="accordion-section__items">
         {items.map((item) => (
